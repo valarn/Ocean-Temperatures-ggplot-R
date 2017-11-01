2d89e7f78b0dd4d9a7d24165652c1b76edbb4ce4
wjdan94 <walharth@bu.edu>
2017-11-01 10:09:33 -0400

    beautifying

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 # It will do the following:
 #  1- download all the data files from 2001 - 2016
 #  2- creates a directory to save the cleaned data 
-#  3-
+#  3- runs R script
 
 START=$1
 END=$2
@@ -15,12 +15,14 @@ echo "Downloading all data between ${START} - ${END}"
 
 echo ""
 echo "Creating a directory to save clean data"
-
 eval "mkdir ${SAVE_DIR_NAME}"
-
+echo "Clean data will be saved in ./${SAVE_DIR_NAME}"
 echo ""
+
 echo "Running R Script to clean downloaded data"
 RUN_R="Rscript --vanilla ./cleaning_script.R ${START} ${END} ${SAVE_DIR_PATH} ${SAVE_DIR_NAME}"
 echo "${RUN_R}"
+
+echo ""
 eval $RUN_R
 
